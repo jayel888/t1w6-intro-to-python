8cf667daf38902d5bd2d7926008b342c32287a3b
jayel888 <15644@coderacademy.edu.au>
2024-06-04 20:45:04 +1000

    variables

---
+++ another_main.py
@@ -0,0 +1,31 @@
+# Variables 
+# first_name snake_case for python variables
+# camelCase for JavaScript variable
+# PascalCase for classes in all the languages
+# kebab-case 
+
+num1 = 5
+num2 = 7
+print(num1 + num2)
+
+num2 = 1
+
+print(num1 + num2)
+
+# assign the value of num2 to num1
+# num1 <- num2
+num1 = num2
+
+print(num1 + num2)
+
+num3 = 5
+
+num3 = num3 + 2
+print(num3)
+
+num3+=3 # num3 = num3 + 2
+print(num3) 
+total = num1 + num2 + num3
+# String interpolation
+print("The total addition is",total)
+print(f"The total addition is {total}")
