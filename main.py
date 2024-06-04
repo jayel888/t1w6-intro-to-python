e12445bac2d3ed1fd38cf3347f12a6cce0bd0a0f
jayel888 <15644@coderacademy.edu.au>
2024-06-04 20:29:28 +1000

    added data types

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
-print("Hell world!!")
-print(5-3)
-print("Don't forget to commit!")
+# print("Hell world!!")
+# print(5-3)
+# print("Don't forget to commit!")
+
+# print("it's a comment, it won't be printed")
+# ctrl+/ for shortcut
+
+# Data types 
+# Integers 
+# 4
+# 7
+# 9
+# integer
+# print(type(9)) 
+# string
+# print(type("9"))
+
+# Floats
+# 2.5
+# 5.347652
+# print(4.5456543)
+# print(type(-6.5463))
+# print(type(-6))
+
+# Complex (imaginary numbers), not covered in the course
+# 2 + 3j
+# print(type(4 + 7j))
+
+# Operators + - * / %
+# print(4 * 5)
+# print(15/3)
+# print(15/2)
+# print(7 % 2)
+
+# String
+# "Hello world"
+# 'Hello world'
+# print("this is a string")
+
+# String  concantenations, addition, join
+# print("hello " + "world")
+# print(3 + 5)
+# print("3" + "5")
+
+# Boolean
+True
+False
+# print(type(False))
+0 # Falsy value
+"" # Falsy value
+# print(bool(0))
+# print(bool(""))
+# print(bool(-123))
+# print(bool("False"))
+
+# comparison operators <, >, <=, >=, ==, !=
+# print(2 < 3)
+# print(5 >= 5)
+# print("HELLO".lower())
+# print("Hello".lower() == "hello")
+
+# Logical operators and, or, not
+print(True and False) # AND returns true when both operators are true
+print(False or True) # OR returns True, when  one or both of the operators are true
+print(not False) # NOT returns the opposite value
