a4332a4389f8b5b2082eadd7ef9f543772897ba9
jayel888 <15644@coderacademy.edu.au>
2024-06-04 21:02:12 +1000

    created scripts with input

---
+++ area_of_square.py
@@ -0,0 +1,7 @@
+# Get the length from the user
+length = float(input("What is the length of the side?"))
+print(length)
+#calculate and store the area in a variable 
+area = length * length
+# Print the calculated area to the terminal
+print(f"the calculated area of the square is {area}cm^2")
---
+++ create_email_form.py
@@ -0,0 +1,10 @@
+# Jess Lee Coder Academy -> jess.lee@coderacademy.com.au
+
+first_name = input("Please enter your first name:")
+last_name = input("Please enter your last name:")
+company_name = input("Please enter your company name (without spaces):")
+
+print(f"Your email address is: {first_name}.{last_name}@{company_name}.com".lower())
+
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)
